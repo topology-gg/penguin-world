aa3996963d9ee60cc8c236061444148b6c1efb83
droak <me+git@droak.sh>
2024-05-22 23:33:18 +0100

    better initial screen; strip more stuff

--- client/client/src/scenes/Game.ts
+++ client/client/src/scenes/Game.ts
@@ -5,45 +5,32 @@ import config from "../config";
 import CRDT from "../networking/crdt";
 import Media from "../networking/media";
 import DefaultButton from "../ui-components/defaultButton";
-import { PeerInfo } from "./types";
-
-interface connectionContainer {
-  yourId: IText;
-  otherId: IText;
-  connectButton: DefaultButton;
-}
 
 export default class Demo extends Phaser.Scene {
+  private username = "";
+  private lobbyInputText: InputText | undefined;
+  private removeSignalEvent: any = () => {};
+
   constructor() {
     super("GameScene");
   }
 
-  private _connectButton: DefaultButton;
-
-  private dialogue: InputText | undefined;
-  private username = "";
-  private inputText: InputText | undefined;
-  private lobbyInputText: InputText | undefined;
-
-  private connectionContainers: connectionContainer[] = [];
-
-  private removeSignalEvent: any = () => {};
-
   preload() {}
 
   create() {
-    this.renderAcceptButton();
     this.renderWelcomeMessage();
     this.renderInputText();
     this.renderLobbyInputText();
+    this.renderPlatformerButton();
   }
+
   renderPlatformerButton() {
     var connectButton = this.add.existing(
       new DefaultButton(
         this,
-        "Enter Platformer",
+        "Connect",
         (config.scale.width / 4) * 2,
-        config.scale.height / 2 + 100,
+        config.scale.height / 2 + 50,
         config.scale.width / 4 - 10,
         "large",
         () => {
@@ -60,8 +47,8 @@ export default class Demo extends Phaser.Scene {
             crdt: crdt,
             media: media,
           });
-        }
-      )
+        },
+      ),
     );
 
     return connectButton;
@@ -70,11 +57,11 @@ export default class Demo extends Phaser.Scene {
   renderWelcomeMessage() {
     var welcomeMessageConfig: IText.IConfig = {
       text: "Welcome to esotere",
-      color: "black",
+      color: "white",
       border: 1,
-      backgroundColor: "white",
       readOnly: true,
       align: "center",
+      fontSize: "50px",
     };
     var welcomeMessage = new InputText(
       this,
@@ -82,11 +69,9 @@ export default class Demo extends Phaser.Scene {
       config.scale.height / 2 - 200,
       500,
       50,
-      welcomeMessageConfig
+      welcomeMessageConfig,
     );
     this.add.existing(welcomeMessage);
-
-    this.dialogue = welcomeMessage;
   }
 
   renderInputText() {
@@ -103,9 +88,8 @@ export default class Demo extends Phaser.Scene {
       config.scale.height / 2 - 100,
       500,
       50,
-      inputTextConfig
+      inputTextConfig,
     );
-    inputText.setStyle("margin-top", "5px");
 
     this.add.existing(inputText);
 
@@ -124,133 +108,15 @@ export default class Demo extends Phaser.Scene {
     var lobbyInputText = new InputText(
       this,
       (config.scale.width / 4) * 2,
-      config.scale.height / 2 - 50,
+      config.scale.height / 2 - 25,
       500,
       50,
-      lobbyInputTextConfig
+      lobbyInputTextConfig,
     );
-
-    lobbyInputText.setStyle("margin-top", "5px");
 
     this.add.existing(lobbyInputText);
 
     // Set our lobby input text as a member object
     this.lobbyInputText = lobbyInputText;
-  }
-
-  renderAcceptButton() {
-    this._connectButton = this.add.existing(
-      new DefaultButton(
-        this,
-        "Confirm",
-        (config.scale.width / 4) * 2,
-        config.scale.height / 2,
-        config.scale.width / 4 - 10,
-        "large",
-        () => {
-          let newUsername = this.inputText?.text || "";
-
-          if (newUsername.length > 0) {
-            this.username = newUsername;
-            this.dialogue?.setText(`Username : ${newUsername}`);
-            this._connectButton.destroy();
-            this.inputText?.destroy();
-            this.renderPlatformerButton();
-          }
-        }
-      )
-    );
-  }
-
-  renderConnectButton() {
-    var connectButton = this.add.existing(
-      new DefaultButton(
-        this,
-        "Connect",
-        (config.scale.width / 4) * 2,
-        config.scale.height / 2 + 200,
-        config.scale.width / 4 - 10,
-        "large",
-        () => {
-          let index = this.connectionContainers.length;
-          this.connect(index - 1);
-        }
-      )
-    );
-
-    return connectButton;
-  }
-
-  renderInitializeConnectButton() {
-    var connectButton = this.add.existing(
-      new DefaultButton(
-        this,
-        "+ Initialize Connection",
-        (config.scale.width / 4) * 2 - 150,
-        config.scale.height / 2 - 100,
-        config.scale.width / 4 - 10,
-        "large",
-        () => {
-          this.renderConnection(true);
-        }
-      )
-    );
-
-    return connectButton;
-  }
-
-  renderRespondToConnectButton() {
-    var connectButton = this.add.existing(
-      new DefaultButton(
-        this,
-        "+ Respond to Connection",
-        (config.scale.width / 4) * 2 + 150,
-        config.scale.height / 2 - 100,
-        config.scale.width / 4 - 10,
-        "large",
-        () => {
-          this.renderConnection(false);
-        }
-      )
-    );
-
-    return connectButton;
-  }
-
-  private attachSignalEvent({ peer, index }: PeerInfo) {
-    let username = this.username;
-
-    let connectionContainer = this.connectionContainers[index];
-
-    function signalData(data: any) {
-      //@ts-ignore
-      data.username = username;
-
-      if (connectionContainer.yourId.text.length == 0) {
-        connectionContainer?.yourId?.setText(JSON.stringify(data));
-      }
-    }
-
-    this.removeSignalEvent = () => peer.removeListener("signal", signalData);
-    peer.on("signal", signalData);
-  }
-
-  initializePeer(is_initiator: boolean) {
-    this.game.config.connections.initializePeer(
-      is_initiator,
-      true,
-      // bind the Game scene object to the `this` keyword so it is accessible in the callback function
-      this.attachSignalEvent.bind(this)
-    );
-  }
-
-  connect(index: number) {
-    console.log(this);
-    let selectedPeer = this.game.config.connections.getConnectionByIndex(index);
-    let connectionContainer = this.connectionContainers[index];
-
-    let username = JSON.parse(connectionContainer.otherId.text).username;
-    this.game.config.connections.updateUsername(index, username);
-    selectedPeer.peer.signal(connectionContainer.otherId.text);
   }
 }
